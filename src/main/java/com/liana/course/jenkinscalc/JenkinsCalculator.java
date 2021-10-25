4dec7d999687bfc8edf7bd1def2043ea13841374
Liana Beglaryan <beglaryanliana@gmail.com>
2021-10-26 02:39:41 +0400

    initial commit

---
+++ src/main/java/com/liana/course/jenkinscalc/JenkinsCalculator.java
@@ -0,0 +1,19 @@
+package com.liana.course.jenkinscalc;
+
+public class JenkinsCalculator {
+	
+		// Method to add 2 numbers
+		public int addNumbers(int numberOne, int numberTwo)
+		{
+		return numberOne + numberTwo;
+		} 
+
+		// Method to add 2 numbers
+
+		public int substractNumbers(int numberOne, int numberTwo)
+		{
+		return numberOne - numberTwo;
+		}
+		
+
+}
---
+++ src/test/java/com/liana/course/jenkinscalc/JenkinsCalctest.java
@@ -0,0 +1,20 @@
+package com.liana.course.jenkinscalc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsCalctest {
+
+	@Test
+	public void addTest() {
+	JenkinsCalculator myCalc = new JenkinsCalculator();
+	assertEquals(10,myCalc.addNumbers(5,5));
+	}
+
+	@Test
+	public void subtractTest() {
+	JenkinsCalculator myCalc = new JenkinsCalculator();
+	assertEquals(5,myCalc.substractNumbers(10,5));
+	}
+}
